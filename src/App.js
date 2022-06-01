a312d30c1b6a79ca1479e56be434834df5b0d7c9
Nariyuki Saito <toshincompos@gmail.com>
2022-06-02 01:58:12 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -90,8 +90,7 @@ export default function App() {
         </div>
 
         <div className="bio">
-          I am farza and I worked on self-driving cars so that's pretty cool
-          right? Connect your Ethereum wallet and wave at me!
+          I am nakaakist. Connect your Ethereum wallet and wave at me!
         </div>
 
         <div className="totalWaves">
